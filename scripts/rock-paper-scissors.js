b7f106f531efd9c50316ad04c4f3cfb45cba068c
marco-tav <marco.tavella@outlook.com>
2021-10-11 18:31:46 -0300

    Game is finished. Added code that prevents wrong user input (like typos) to break the game.

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,16 +1,19 @@
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------- FUNCTIONS ------------------------------------------------------------------------------
+
 function numberOfRounds() {
     n = prompt("How many rounds would you like to play?", " ");
     return n;
 }
 
 function computerPlay() {
-    //Pick a random number between 0, 1 and 2.
+    // Pick a random number between 0, 1 and 2.
     let randomNumber;
     randomNumber = Math.floor(Math.random() * 3);
 
     let computerSelection;
 
-    //Make an equivalence between each of those numbers and rock, paper or scissors.
+    // Make an equivalence between each of those numbers and rock, paper or scissors.
     switch(randomNumber) {
         case 0:
             computerSelection = 'ROCK';
@@ -29,15 +32,17 @@ function computerPlay() {
 function playerPlay() {
     let playerSelection;
     playerSelection = prompt('Choose your hand!', '');
+
+    // Added trim to prevent whitespace and to make the choice case insensitive.
     playerSelection = playerSelection.trim();
     playerSelection = playerSelection.toUpperCase();
-    
+
     return playerSelection;
 }
 
 function playRound(playerSelection, computerSelection, playerScore, computerScore) {
     let message;
-    if (playerSelection === 'ROCK') {
+    if (playerSelection === 'ROCK') { // Comparison cases when player selected ROCK.
         if (computerSelection === 'ROCK') {
             message = `You chose ${playerSelection}. The computer chose ${computerSelection}. This round is tied.`;
             playerScore += 1;
@@ -50,7 +55,7 @@ function playRound(playerSelection, computerSelection, playerScore, computerScor
             playerScore += 1;
         }
         
-    }   else if (playerSelection === 'PAPER') {
+    }   else if (playerSelection === 'PAPER') { // Comparison cases when player selected PAPER.
             if (computerSelection === 'ROCK') {
                 message = `You chose ${playerSelection}. The computer chose ${computerSelection}. You win this round.`;
                 playerScore += 1;
@@ -63,7 +68,7 @@ function playRound(playerSelection, computerSelection, playerScore, computerScor
                 computerScore += 1;
             }
         
-    }   else if (playerSelection === 'SCISSORS') {
+    }   else if (playerSelection === 'SCISSORS') { // Comparison cases when player selecter SCISSORS.
             if (computerSelection === 'ROCK') {
                 message = `You chose ${playerSelection}. The computer chose ${computerSelection}. You lose this round.`;
                 computerScore += 1;
@@ -83,11 +88,21 @@ function playRound(playerSelection, computerSelection, playerScore, computerScor
 function playGame(numberOfRounds) {
     i = 0;
     
-    let result = ["string", 0, 0];  
+    // Create an empty variable for playRound() function to fill in.
+    let result;
+    result = ["string", 0, 0];  
 
+    // Plays the number of rounds that the player entered.
     while (i < numberOfRounds) {
-        computerChoice = computerPlay();
         playerChoice = playerPlay();
+        
+        // While block to correct typos and incorrect inputs.
+        while (playerChoice !== "ROCK" && playerChoice !== "PAPER" && playerChoice !== "SCISSORS") {
+            alert(`You entered ${playerChoice}. Only ROCK, PAPER and SCISSORS will work. Try again!`)
+            playerChoice = playerPlay();
+        }
+        
+        computerChoice = computerPlay();
         result = playRound(playerChoice, computerChoice, result[1], result[2]);
         alert(`${result[0]} Player: ${result[1]}, Computer: ${result[2]}.`)
         i += 1;
@@ -99,6 +114,7 @@ function playGame(numberOfRounds) {
 function compareScores(scorePlayer, scoreComputer) {
     let finalMessage;
     
+    // Compares the final scores and returns a victory, defeat or tie message.
     if (scorePlayer > scoreComputer) {
         finalMessage = `Congratulations, you won this game ${scorePlayer} to ${scoreComputer}!`;
     } else if (scorePlayer < scoreComputer) {
@@ -110,11 +126,18 @@ function compareScores(scorePlayer, scoreComputer) {
     return finalMessage;
 }
 
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------- EXECUTION ----------------------------------------------------------------------------
+
 do {
+    let num;
     num = numberOfRounds(); 
 
+    // scoreBoard is an array like [player's score, computer's score].
     let scoreBoard;
     scoreBoard = playGame(num);
+    
     theFinalMessage = compareScores(scoreBoard[0], scoreBoard[1]);
     alert(theFinalMessage);
+
 } while (confirm("Do you wish to play again?"));
