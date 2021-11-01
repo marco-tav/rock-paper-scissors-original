97954bcb83d8b3da0c86cf1585631c47c62b285a
marco-tav <marco.tavella@outlook.com>
2021-11-01 15:21:43 -0300

    Modify playerPlay function (and change to descriptive name). Add code so rock, paper, scissors buttons work

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,5 +1,11 @@
 //--------------------------------------------------------------------------------------------------------------------
 //---------------------------------------------------- FUNCTION DECLARATIONS -----------------------------------------
+
+function numberOfRounds() {  // Prompts the player for amount of rounds to play.
+    n = prompt("How many rounds would you like to play?", " ");
+    return n;
+}
+
 function getComputerSelection() {   // Gets the computer selection.
     
     // Pick a random number between 0, 1 and 2.
@@ -24,26 +30,14 @@ function getComputerSelection() {   // Gets the computer selection.
     return computerSelection;
 }
 
-function getPlayerSelection() { 
-    const buttons = document.querySelectorAll('.rps');
-
-    let id;
-    let playerSelection;
-
-    buttons.forEach((button) => {
-
-        button.addEventListener('click', () => {
-            id = button.id;
-
-            if (id === 'rock') {
-                playerSelection = 'ROCK';
-            } else if (id === 'paper') {
-                playerSelection = 'PAPER';
-            } else if (id === 'scissors') {
-                playerSelection = 'SCISSORS';
-            }
-        })
-    })
+function getPlayerSelection(id) {
+    if (id === 'rock') {
+        playerSelection = 'ROCK';
+    } else if (id === 'paper') {
+        playerSelection = 'PAPER';
+    } else if (id === 'scissors') {
+        playerSelection = 'SCISSORS';
+    }
 
     return playerSelection;
 }
@@ -84,5 +78,62 @@ function playRound(playerSelection, computerSelection, playerScore, computerScor
     return [message, playerScore, computerScore];
 }
 
+function playGame(numberOfRounds) {  // Plays a game with the amount of rounds the player entered.
+    i = 0;
+    
+    // Create an array for playRound() function to fill.
+    let result;
+    result = ["string", 0, 0];  
+
+    // A while loop the number of rounds that the player entered.
+    while (i < numberOfRounds) {
+        playerChoice = playerPlay();
+        
+        while (playerChoice !== "ROCK" && playerChoice !== "PAPER" && playerChoice !== "SCISSORS") {
+            alert(`You entered ${playerChoice}. Only ROCK, PAPER and SCISSORS will work. Try again!`)
+            playerChoice = playerPlay(); 
+        }
+        
+        computerChoice = computerPlay();
+        
+        result = playRound(playerChoice, computerChoice, result[1], result[2]);
+        alert(`${result[0]} Player: ${result[1]}, Computer: ${result[2]}.`)
+        
+        i += 1;
+    }
+
+    return [result[1], result[2]];
+}
+
+function compareScores(scorePlayer, scoreComputer) {
+    let finalMessage;
+    
+    // Compares the final scores and returns a victory, defeat or tie message.
+    if (scorePlayer > scoreComputer) {
+        finalMessage = `Congratulations, you won this game ${scorePlayer} to ${scoreComputer}!`;
+    
+    } else if (scorePlayer < scoreComputer) {
+        finalMessage = `Oh no, you lost ${scorePlayer} to ${scoreComputer} to the computer! Better luck next time!`;
+    
+    } else if (scorePlayer === scoreComputer) {
+        finalMessage = `It's a tie at ${scorePlayer} to ${scoreComputer}!`;
+    }
+
+    return finalMessage;
+}
+
 // ---------------------------------------------------------------------------------------------------------------------
 // ---------------------------------------------------------- EXECUTION ------------------------------------------------
+
+const buttons = document.querySelectorAll('.rps');
+
+let buttonId;
+let playerSelection;
+
+buttons.forEach((button) => {
+    
+    button.addEventListener('click', () => {
+        buttonId = button.id;
+        playerSelection = getPlayerSelection(buttonId);
+    })
+})
